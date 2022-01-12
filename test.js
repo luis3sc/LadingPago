479cd78d5020b37f3a721555ca87f492a587eca0
luis3sc <33030471+luis3sc@users.noreply.github.com>
2022-01-12 00:56:15 -0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,72 @@
+(function () {
+
+    'use strict';
+  
+    // breakpoint where swiper will be destroyed
+    // and switches to a dual-column layout
+    const breakpoint = window.matchMedia('(min-width:620px)');
+  
+    // keep track of swiper instances to destroy later
+    let mySwiper;
+  
+    //////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
+  
+    const breakpointChecker = function () {
+  
+      // if larger viewport and multi-row layout needed
+      if (breakpoint.matches === true) {
+  
+        // clean up old instances and inline styles when available
+        if (mySwiper !== undefined) mySwiper.destroy(true, true);
+  
+        // or/and do nothing
+        return;
+  
+        // else if a small viewport and single column layout needed
+      } else if (breakpoint.matches === false) {
+  
+        // fire small viewport version of swiper
+        return enableSwiper();
+  
+      }
+  
+    };
+  
+    //////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
+  
+    const enableSwiper = function () {
+  
+      mySwiper = new Swiper('.container', {
+  
+        loop: true,
+  
+        slidesPerView: 'auto',
+        
+        centeredSlides: true,
+  
+        a11y: true,
+        keyboardControl: true,
+        grabCursor: true });
+  
+  
+  
+  
+    };
+  
+    //////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
+  
+    // keep an eye on viewport size changes
+    breakpoint.addListener(breakpointChecker);
+  
+    // kickstart
+    breakpointChecker();
+  
+  
+  
+  })(); /* IIFE end */
